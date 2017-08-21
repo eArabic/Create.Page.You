0aaaa85a07fdaf72cb03a72f3431aed8edace729
internetperson <mike_novellino4biz@aol.com>
2017-08-21 10:52:12 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head><title>TextPaste</title></head>
+<body>
+  <h1>TextPaste: 
+A simple way to create and share .txt files.</h1>
+  <form method="post" action="">
+    <input type="text" name="typed" value="<?= isset($_POST['typed']) ? htmlspecialchars($_POST['typed']) : '' ?>" />
+    <input type="submit" name="Paste!" />
+  </form>
+
+<?php
+
+$seed = str_split('abcdefghijklmnopqrstuvwxyz'
+                 .'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                 .'0123456789');
+shuffle($seed);
+$rand = '';
+foreach (array_rand($seed, 5) as $k) $rand .= $seed[$k];
+
+if(isset($_POST['typed'])) {
+  $my_file = $rand.'.txt';
+  $handle = fopen($my_file, 'w') or die('Cannot open file: '.$my_file);
+  $txt = htmlspecialchars($_POST['typed']);
+  fwrite($handle, $txt);
+  echo 'http://internetperson.16mb.com/textpaste/'.$my_file.' - Latest file you created.';
+}
+?>
+
