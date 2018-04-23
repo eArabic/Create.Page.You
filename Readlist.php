503ba243deb0bd2ca84fde48cd379c743d6a8c49
eArabic <arabic40@icloud.com>
2018-04-23 13:41:06 +0300

    Update Readlist.php

--- Readlist.php
+++ Readlist.php
@@ -1,7 +1,10 @@
 <?php
 $fileList = glob('Data/*');
+$str = str_replace( array('-', '_'), ' ', $page); 
+$fileList = glob('Data/*');
 foreach($fileList as $filename){
-    echo '<hr><a href="'.$filename.'">  Open File  </a> ';
-   echo $filename, '<br>'; 
+    $List = str_replace( array( 'Data/', '.html'), '', $filename ); 
+    echo "<hr>".$List, '<br><h1><a href="'.$filename.'">  Open  </a><h1> '; 
+ 
 }
 ?>
