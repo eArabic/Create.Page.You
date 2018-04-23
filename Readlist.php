7f3e45468b3a6a94c0d9d98050dffadefb5a80a0
eArabic <arabic40@icloud.com>
2018-04-23 13:48:44 +0300

    Update Readlist.php

--- Readlist.php
+++ Readlist.php
@@ -1,12 +1,13 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 
 <?php
-$fileList = glob('Data/*');
+$e++; 
 $str = str_replace( array('-', '_'), ' ', $page); 
 $fileList = glob('Data/*');
 foreach($fileList as $filename){
     $List = str_replace( array( 'Data/', '.html'), '', $filename ); 
-    echo "<hr> <h1>".$List, '<a href="'.$filename.'">  Open  </a><h1> '; 
+     echo "<div class=\"container-fluid\">";
+     echo   "<hr> <h1>".$e++."-".$List, '<a href="'.$filename.'">  Open  </a><h1></div> '; 
  
 }
 ?>
